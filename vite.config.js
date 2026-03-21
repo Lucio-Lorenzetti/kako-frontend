135bc8aca287164d70cb1d2532ceae644f7d72cf
Lucio-Lorenzetti <89593565+Lucio-Lorenzetti@users.noreply.github.com>
2026-03-21 15:07:16 -0300

    Correción para Vercel -Creación de archivo vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // permite acceso desde cualquier IP, necesario para ngrok
-    port: 5173,
-    allowedHosts: [
-      'applaudable-reinaldo-unvainly.ngrok-free.dev' // 👈 tu subdominio ngrok actual
-    ],
-  },
 })
