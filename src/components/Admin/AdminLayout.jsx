0597725f5f1ae09e371f685c0c1dfb7030bf65fc
Lucio-Lorenzetti <89593565+Lucio-Lorenzetti@users.noreply.github.com>
2026-09-18 21:21:42 -0300

    feat: mostrar el footer tambien en el panel de Admin
    AdminLayout envuelve Dashboard/Reservas/Usuarios/Turnos, asi que
    alcanza con agregar CopyRight ahi una sola vez para que el footer
    quede presente en todas las paginas del panel.

--- src/components/Admin/AdminLayout.jsx
+++ src/components/Admin/AdminLayout.jsx
@@ -1,5 +1,6 @@
 // src/components/Admin/AdminLayout.jsx
 import Header from "./Header";
+import CopyRight from "../Home/CopyRight";
 
 export default function AdminLayout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function AdminLayout({ children }) {
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="p-6 bg-gray-100 flex-1">{children}</main>
+        <CopyRight />
       </div>
     </div>
   );
